1f5244c24c42f4741bf1fe58b078f81fca34c571
Somewhat-average <matthewraulerson7@gmail.com>
2026-04-23 23:57:20 -0400

    fix silly

--- src/cowrie/commands/wget.py
+++ src/cowrie/commands/wget.py
@@ -1,4 +1,4 @@
-g Copyright (c) 2009 Upi Tamminen <desaster@gmail.com>
+# Copyright (c) 2009 Upi Tamminen <desaster@gmail.com>
 # See the COPYRIGHT file for more information
 
 from __future__ import annotations
